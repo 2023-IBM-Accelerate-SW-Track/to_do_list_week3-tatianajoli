999c0557f37ea23a387519d7b886accb87f52e0e
tatianajoli <tatiana.joli@outlook.com>
2023-06-26 23:55:41 -0400

    completed everything except testing, could only figure out the duplicate method

--- src/App.test.js
+++ src/App.test.js
@@ -20,10 +20,11 @@ test('test that App component renders', () => {
   render(<App />, container);
  });
 
-test('test that new-item-button is a button', () => {
+ test('test that new-item-button is a button', () => {
   render(<App/>, container);
   const element = screen.getByTestId('new-item-button');
-  expect(element.innerHTML.toLowerCase().includes("button")).toBe(true)
+  console.log(element.outerHTML.toLowerCase());
+  expect(element.outerHTML.toLowerCase().includes("button")).toBe(true)
 });
 
 test('test that new-item-input is an input ', () => {
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,48 @@
-import React, { Component } from "react";
-import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
+import React, { Component } from 'react';
+import pfp from "../assets/pfp.jpg";
+import './About.css';
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
-        <div class="split left">
-          <div className="centered">
-            <img
-              className="profile_image"
-              src={profile_pic}
-              alt="Profile Pic"
-            ></img>
+        <p>
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img 
+                className="profile_image"
+                src={pfp}
+                alt="Profile Pic"
+                ></img>
+            </div>
           </div>
-        </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">All About Me!</div>
+              <div className="brief_description">
+                <p>Hello! My name is Tatiana Gonzalez! I am a Rutgers University
+                  - New Brunswick student with an intended graduation date of May 
+                  2025. I am a part of the Honors College, Douglass Residential 
+                  College, and the School of Arts and Sciences. I am majoring in 
+                  Computer Science with a minor in Cognitive Science.</p>
+                  <p>I am a part of many organizations and activities, one being the 
+                  Advancing Women in Computer Science Program, where I learn about 
+                  Computer Science career opportunities through peer leadership and 
+                  portfolio building, practice computational thinking and technical 
+                  skills to prepare for interviews, and work together with like-
+                  minded peers to build confidence and achieve academic success. 
+                  Overall, this program has helped build confidence in my 
+                  professional capabilities and has provided me with the resources 
+                  I need to succeed in interviews and other professional settings.
+                </p>
+              </div>
             </div>
           </div>
         </div>
+
+        </p> 
       </div>
-    );
+    )
   }
 }
